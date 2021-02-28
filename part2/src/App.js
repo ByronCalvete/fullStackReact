44665a1b0a053547c896f9cdb69727684ad8d87f
Byron Calvete <calvete94@gmail.com>
2021-02-28 09:16:42 -0300

    useEffect with fetch

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,15 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles.css';
 import { Note } from './Note.js';
 
-const App = ({ data = [] }) => {
-	const [notes, setNotes] = useState(data);
+const App = () => {
+	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState('');
-	const [showAll, setShowAll] = useState(true);
+	const [loading, setLoading] = useState(false);
 
-	if (typeof data === 'undefined' || data.length === 0) {
-		return 'No tenemos notas que mostrar';
-	}
+	// if (typeof data === 'undefined' || data.length === 0) {
+	// 	return 'No tenemos notas que mostrar';
+	// }
+
+	useEffect(() => {
+		console.log('useEffect');
+		setLoading(true);
+		setTimeout(() => {
+			fetch('https://jsonplaceholder.typicode.com/posts')
+				.then((response) => response.json())
+				.then((json) => {
+					setNotes(json);
+					setLoading(false);
+				});
+		}, 1000);
+	}, []);
 
 	const handleChange = (event) => {
 		setNewNote(event.target.value);
@@ -19,32 +32,24 @@ const App = ({ data = [] }) => {
 		event.preventDefault();
 		const noteToAddToState = {
 			id: notes.length + 1,
-			content: newNote,
-			date: new Date().toISOString(),
-			important: Math.random() < 0.5,
+			title: newNote,
+			body: newNote,
 		};
 
 		setNotes([...notes, noteToAddToState]);
 		setNewNote('');
 	};
 
-	const handleShowAll = () => {
-		setShowAll(() => !showAll);
-	};
-
 	return (
 		<div>
 			<h1>Notes</h1>
-			<button onClick={handleShowAll}>
-				{showAll ? 'Show only important' : 'Show all'}
-			</button>
-			<ul>
-				{notes
-					.filter((note) => (showAll ? true : note.important === true))
-					.map((note) => (
-						<Note key={note.id} content={note.content} date={note.date} />
-					))}
-			</ul>
+			{loading && 'Cargando....'}
+			<ol>
+				{notes.map((note) => (
+					<Note key={note.id} title={note.title} body={note.body} />
+					// <Note key={note.id} {...note} />
+				))}
+			</ol>
 			<form onSubmit={handleSubmit}>
 				<input type="text" onChange={handleChange} value={newNote} />
 				<button>Create note</button>
